6e587fd9760cf612fd2af44508d9a91a3c1571eb
mateusSSalomao <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-08-23 11:41:16 -0300

    Exercicios aula 02

---
+++ Aula02Calculadora/Aula02Calculadora/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula02Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1, numero2, div;
+            
+            Console.WriteLine("Digite o primeiro número: ");
+            numero1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número: ");
+            numero2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine($"{numero1} + {numero2} = " + (numero1 + numero2));
+            Console.WriteLine($"{numero1} - {numero2} = " + (numero1 - numero2));
+            Console.WriteLine($"{numero1} * {numero2} = " + (numero1 * numero2));
+
+            div = numero1 / numero2;
+
+            Console.WriteLine($"{numero1} / {numero2} = {div:F2}");
+            Console.WriteLine($"{numero1} ^ {numero2} = " + Math.Pow(numero1, numero2));
+            Console.WriteLine($"√{numero1} = " + Math.Sqrt(numero1));
+        }
+    }
+}
---
+++ Aula02Calculadora/EntradaSaidaDeOperadores-Exercício1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EntradaSaidaDeOperadores_Exercício1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float salarioAtg, abono, salarioNv;
+            Console.WriteLine("Digite o antigo sálario do funcionario: ");
+            salarioAtg = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Digite o Abono do funcionario: ");
+            abono = Convert.ToSingle(Console.ReadLine());
+
+            salarioNv = salarioAtg + abono;
+            Console.WriteLine("O novo salário será: "+abono);
+        }
+    }
+}
