861d1d39f8569186176b4cd83040f024c29f6cde
mateusSSalomao <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-08-28 16:50:01 -0300

    Aula05

--- Aula05/Aula05-Exercicio3/Program.cs
+++ Aula05/Aula05-Exercicio3/Program.cs
@@ -4,21 +4,24 @@
     {
         static void Main(string[] args)
         {
-            int[,] matrix = { {1,2,3 },{4,5,6 },{7,8,9 } };
-            int somaDiagonal = 0, somaDiagonalReversa = 0;
+            int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int somaDiagonal = 0, somaDiagonalReversa = 0; string Diagonal = " ",DiagonalReversa = " ";
 
-            for (int i = 0; i < Math.Sqrt(matrix.Length); i ++)
+            for (int i = 0; i < Math.Sqrt(matrix.Length); i++)
             {
-                Console.Write($"{matrix[i,i]} \n{matrix[i, 2 - i]} \n");
+                Diagonal += matrix[i, i] + " ";
+                DiagonalReversa += matrix[i, 2 - i] + " ";
                 somaDiagonal += matrix[i, i];
                 somaDiagonalReversa += matrix[i, 2 - i];
             }
             Console.WriteLine(
+                $"Os valores na diagonal principal é: {Diagonal}\n"+
+                $"Os valores na diagonal secundaria é: {DiagonalReversa}\n"+
                 $"A soma da Diagonal principal é: {somaDiagonal} \n" +
                 $"A soma da Diagonal secundaria é: {somaDiagonalReversa}");
 
-          
-           
+
+
         }
     }
 }
