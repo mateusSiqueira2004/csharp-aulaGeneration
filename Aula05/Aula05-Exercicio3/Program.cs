28c7098eca05ba46168491e719a89597a6e7a1a0
mateusSSalomao <118471492+mateusSiqueira2004@users.noreply.github.com>
2023-08-28 16:29:13 -0300

    Aula05

--- Aula05/Aula05-Exercicio3/Program.cs
+++ Aula05/Aula05-Exercicio3/Program.cs
@@ -7,7 +7,7 @@
             int[,] matrix = { {1,2,3 },{4,5,6 },{7,8,9 } };
             int somaDiagonal = 0, somaDiagonalReversa = 0;
 
-            for (int i = 0; i < 3; i ++)
+            for (int i = 0; i < Math.Sqrt(matrix.Length); i ++)
             {
                 Console.Write($"{matrix[i,i]} \n{matrix[i, 2 - i]} \n");
                 somaDiagonal += matrix[i, i];
